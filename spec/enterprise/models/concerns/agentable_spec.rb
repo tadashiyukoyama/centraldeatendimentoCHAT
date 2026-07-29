23595ac52099578067659458ea5aee61f33f86fa
Bellartejundiai <bellartecomercial@gmail.com>
2026-07-29 01:06:51 -0300

    test: follow configured Nemmo model default

--- spec/enterprise/models/concerns/agentable_spec.rb
+++ spec/enterprise/models/concerns/agentable_spec.rb
@@ -213,7 +213,7 @@ RSpec.describe Concerns::Agentable do
       create(:installation_config, name: 'CAPTAIN_OPEN_AI_MODEL', value: 'gpt-4.1-nano')
       account.enable_features!('captain_integration_v2')
 
-      expect(dummy_instance.send(:agent_model)).to eq('gpt-5.2')
+      expect(dummy_instance.send(:agent_model)).to eq(Llm::Models.default_model_for('assistant'))
       expect(account.reload.captain_models).to be_nil
     end
 
